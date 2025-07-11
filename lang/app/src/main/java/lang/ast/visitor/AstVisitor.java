eee212d405b6cd7f828d076478a062d6d26f9fe2
Utkarsh Priyadarshi <utkarshpriyadarshi5026@gmail.com>
2025-07-11 07:53:55 +0530

    Add AST visitor classes: Introduce AstVisitor interface and BaseAstVisitor abstract class to implement the Visitor pattern for AST nodes. This addition facilitates separation of concerns, extensibility, and type safety in handling various AST node types, providing default implementations for visitor methods.

---
+++ lang/app/src/main/java/lang/ast/visitor/AstVisitor.java
@@ -0,0 +1,72 @@
+package lang.ast.visitor;
+
+import lang.ast.statements.*;
+import lang.ast.expressions.*;
+import lang.ast.literals.*;
+import lang.ast.base.*;
+
+/**
+ * Visitor interface for AST nodes.
+ * 
+ * The Visitor pattern allows us to define operations on AST nodes
+ * without modifying the node classes. This is crucial for:
+ * 1. Separation of concerns - parsing vs evaluation vs optimization
+ * 2. Extensibility - adding new operations without changing AST
+ * 3. Type safety - compile-time checking of operations
+ * 
+ * Each AST node type has a corresponding visit method.
+ */
+public interface AstVisitor<T> {
+
+    // Program
+    T visitProgram(Program program);
+
+    // Statements
+    T visitLetStatement(LetStatement letStatement);
+
+    T visitConstStatement(ConstStatement constStatement);
+
+    T visitReturnStatement(ReturnStatement returnStatement);
+
+    T visitExpressionStatement(ExpressionStatement expressionStatement);
+
+    T visitBlockStatement(BlockStatement blockStatement);
+
+    T visitWhileStatement(WhileStatement whileStatement);
+
+    T visitForStatement(ForStatement forStatement);
+
+    T visitBreakStatement(BreakStatement breakStatement);
+
+    T visitContinueStatement(ContinueStatement continueStatement);
+
+    // Expressions
+    T visitIdentifier(Identifier identifier);
+
+    T visitInfixExpression(InfixExpression infixExpression);
+
+    T visitPrefixExpression(PrefixExpression prefixExpression);
+
+    T visitBooleanExpression(BooleanExpression booleanExpression);
+
+    T visitIfExpression(IfExpression ifExpression);
+
+    T visitCallExpression(CallExpression callExpression);
+
+    T visitIndexExpression(IndexExpression indexExpression);
+
+    T visitAssignmentExpression(AssignmentExpression assignmentExpression);
+
+    // Literals
+    T visitIntegerLiteral(IntegerLiteral integerLiteral);
+
+    T visitStringLiteral(StringLiteral stringLiteral);
+
+    T visitArrayLiteral(ArrayLiteral arrayLiteral);
+
+    T visitHashLiteral(HashLiteral hashLiteral);
+
+    T visitFunctionLiteral(FunctionLiteral functionLiteral);
+
+    T visitFStringLiteral(FStringLiteral fStringLiteral);
+}
---
+++ lang/app/src/main/java/lang/ast/visitor/BaseAstVisitor.java
@@ -0,0 +1,137 @@
+package lang.ast.visitor;
+
+import lang.ast.statements.*;
+import lang.ast.expressions.*;
+import lang.ast.literals.*;
+import lang.ast.base.*;
+
+/**
+ * Abstract base class for visitors that provides default implementations.
+ * Subclasses only need to override the methods they care about.
+ */
+public abstract class BaseAstVisitor<T> implements AstVisitor<T> {
+
+    protected T defaultResult() {
+        return null;
+    }
+
+    @Override
+    public T visitProgram(Program program) {
+        return defaultResult();
+    }
+
+    @Override
+    public T visitLetStatement(LetStatement letStatement) {
+        return defaultResult();
+    }
+
+    @Override
+    public T visitConstStatement(ConstStatement constStatement) {
+        return defaultResult();
+    }
+
+    @Override
+    public T visitReturnStatement(ReturnStatement returnStatement) {
+        return defaultResult();
+    }
+
+    @Override
+    public T visitExpressionStatement(ExpressionStatement expressionStatement) {
+        return defaultResult();
+    }
+
+    @Override
+    public T visitBlockStatement(BlockStatement blockStatement) {
+        return defaultResult();
+    }
+
+    @Override
+    public T visitWhileStatement(WhileStatement whileStatement) {
+        return defaultResult();
+    }
+
+    @Override
+    public T visitForStatement(ForStatement forStatement) {
+        return defaultResult();
+    }
+
+    @Override
+    public T visitBreakStatement(BreakStatement breakStatement) {
+        return defaultResult();
+    }
+
+    @Override
+    public T visitContinueStatement(ContinueStatement continueStatement) {
+        return defaultResult();
+    }
+
+    @Override
+    public T visitIdentifier(Identifier identifier) {
+        return defaultResult();
+    }
+
+    @Override
+    public T visitInfixExpression(InfixExpression infixExpression) {
+        return defaultResult();
+    }
+
+    @Override
+    public T visitPrefixExpression(PrefixExpression prefixExpression) {
+        return defaultResult();
+    }
+
+    @Override
+    public T visitBooleanExpression(BooleanExpression booleanExpression) {
+        return defaultResult();
+    }
+
+    @Override
+    public T visitIfExpression(IfExpression ifExpression) {
+        return defaultResult();
+    }
+
+    @Override
+    public T visitCallExpression(CallExpression callExpression) {
+        return defaultResult();
+    }
+
+    @Override
+    public T visitIndexExpression(IndexExpression indexExpression) {
+        return defaultResult();
+    }
+
+    @Override
+    public T visitAssignmentExpression(AssignmentExpression assignmentExpression) {
+        return defaultResult();
+    }
+
+    @Override
+    public T visitIntegerLiteral(IntegerLiteral integerLiteral) {
+        return defaultResult();
+    }
+
+    @Override
+    public T visitStringLiteral(StringLiteral stringLiteral) {
+        return defaultResult();
+    }
+
+    @Override
+    public T visitArrayLiteral(ArrayLiteral arrayLiteral) {
+        return defaultResult();
+    }
+
+    @Override
+    public T visitHashLiteral(HashLiteral hashLiteral) {
+        return defaultResult();
+    }
+
+    @Override
+    public T visitFunctionLiteral(FunctionLiteral functionLiteral) {
+        return defaultResult();
+    }
+
+    @Override
+    public T visitFStringLiteral(FStringLiteral fStringLiteral) {
+        return defaultResult();
+    }
+}

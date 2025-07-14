4a2a76b4be66ea5c8c4ce070360d5c4ea8023690
Utkarsh Priyadarshi <utkarshpriyadarshi5026@gmail.com>
2025-07-14 10:09:15 +0530

    Add prefix expression parsers for boolean, identifier, integer, null, and string literals: Introduce new classes (BooleanLiteralParser, IdentifierExpressionParser, IntegerLiteralParser, NullLiteralParser, StringLiteralParser) to handle respective literal expressions. Each parser implements the PrefixExpressionParser interface, enhancing the parser's capability to manage various expression types effectively.

---
+++ lang/app/src/main/java/lang/parser/expressions/prefix/BooleanLiteralParser.java
@@ -0,0 +1,25 @@
+package lang.parser.expressions.prefix;
+
+import lang.parser.interfaces.PrefixExpressionParser;
+import lang.parser.core.ParsingContext;
+import lang.ast.base.Expression;
+import lang.ast.expressions.BooleanExpression;
+import lang.token.TokenType;
+import lang.token.Token;
+import java.util.Set;
+
+public class BooleanLiteralParser implements PrefixExpressionParser {
+
+    @Override
+    public Expression parsePrefix(ParsingContext context) {
+        Token currToken = context.getTokenStream().getCurrentToken();
+        context.consumeCurrentToken(currToken.type());
+
+        return new BooleanExpression(currToken, currToken.type() == TokenType.TRUE);
+    }
+
+    @Override
+    public Set<TokenType> getHandledTokenTypes() {
+        return Set.of(TokenType.TRUE, TokenType.FALSE);
+    }
+}
---
+++ lang/app/src/main/java/lang/parser/expressions/prefix/IdentifierExpressionParser.java
@@ -0,0 +1,34 @@
+package lang.parser.expressions.prefix;
+
+import lang.parser.interfaces.PrefixExpressionParser;
+import lang.parser.core.ParsingContext;
+import lang.ast.base.Expression;
+import lang.ast.base.Identifier;
+import lang.token.TokenType;
+import lang.token.Token;
+import java.util.Set;
+
+/**
+ * 🔤 IdentifierExpressionParser - Identifier Specialist 🔤
+ * 
+ * Handles identifier expressions.
+ * 
+ * Examples:
+ * - x
+ * - my_variable
+ * - some_function_call
+ * 
+ */
+public class IdentifierExpressionParser implements PrefixExpressionParser {
+
+    @Override
+    public Expression parsePrefix(ParsingContext context) {
+        Token identifierToken = context.consumeCurrentToken(TokenType.IDENTIFIER);
+        return new Identifier(identifierToken, identifierToken.literal());
+    }
+
+    @Override
+    public Set<TokenType> getHandledTokenTypes() {
+        return Set.of(TokenType.IDENTIFIER);
+    }
+}

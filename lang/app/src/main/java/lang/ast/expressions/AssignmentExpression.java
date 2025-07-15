f4d17e27d6c52eb94fc81f46a5c1f3653bbaedcb
Utkarsh Priyadarshi <utkarshpriyadarshi5026@gmail.com>
2025-07-15 06:57:51 +0530

    Enhance AssignmentExpression to support multiple target types: Updated the class to handle simple identifier, array index, and hash key assignments. Added validation methods and improved error handling for target expressions, enhancing robustness and clarity in assignment operations.

--- lang/app/src/main/java/lang/ast/expressions/AssignmentExpression.java
+++ lang/app/src/main/java/lang/ast/expressions/AssignmentExpression.java
@@ -3,11 +3,15 @@ package lang.ast.expressions;
 import lang.ast.base.Expression;
 import lang.ast.base.Identifier;
 import lang.ast.utils.AstCaster;
+import lang.ast.utils.AstValidator;
 import lang.token.Token;
 import lang.ast.visitor.AstVisitor;
 
 /**
- * Represents an assignment expression: x = value
+ * Represents an assignment expression that can handle multiple target types:
+ * - Simple identifier assignment: x = value
+ * - Array index assignment: array[0] = value
+ * - Hash key assignment: hash["key"] = value
  */
 public class AssignmentExpression extends Expression {
     private final Expression target;
@@ -19,12 +23,47 @@ public class AssignmentExpression extends Expression {
         this.value = value;
     }
 
+    /**
+     * Gets the target expression (can be Identifier or IndexExpression)
+     */
+    public Expression getTarget() {
+        return target;
+    }
+
+    /**
+     * Gets the identifier name (only valid when target is an Identifier)
+     * 
+     * @deprecated Use getTarget() and check the type instead
+     */
+    @Deprecated
     public Identifier getName() {
+        if (!AstValidator.isIdentifier(target)) {
+            throw new IllegalStateException(
+                    "Assignment target is not an identifier: " + target.getClass().getSimpleName());
+        }
         return AstCaster.asIdentifier(target);
     }
 
+    /**
+     * Gets the value expression to assign
+     */
     public Expression getValue() {
         return value;
+    }
+
+    /**
+     * Checks if this assignment targets a simple identifier
+     */
+    public boolean isIdentifierAssignment() {
+        return AstValidator.isIdentifier(target);
+    }
+
+    /**
+     * Checks if this assignment targets an index expression (array[i] or
+     * hash["key"])
+     */
+    public boolean isIndexAssignment() {
+        return AstValidator.isIndexExpression(target);
     }
 
     @Override
@@ -38,5 +77,4 @@ public class AssignmentExpression extends Expression {
     public <T> T accept(AstVisitor<T> visitor) {
         return visitor.visitAssignmentExpression(this);
     }
-
 }

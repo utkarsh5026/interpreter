d6f742d0f9b7e4a19d583a769a825b0855b98eb0
Utkarsh Priyadarshi <utkarshpriyadarshi5026@gmail.com>
2025-07-16 08:25:24 +0530

    Implement compound assignment parsing in ExpressionStatementParser: Added support for compound assignment operators, including parsing logic for identifiers and infix expressions, enhancing the expression statement capabilities of the parser.

--- lang/app/src/main/java/lang/parser/parsers/statements/ExpressionStatementParser.java
+++ lang/app/src/main/java/lang/parser/parsers/statements/ExpressionStatementParser.java
@@ -2,12 +2,15 @@ package lang.parser.parsers.statements;
 
 import lang.ast.statements.ExpressionStatement;
 import lang.ast.base.Expression;
+import lang.ast.base.Identifier;
+import lang.ast.expressions.AssignmentExpression;
+import lang.ast.expressions.InfixExpression;
 import lang.parser.core.*;
 import lang.parser.error.ParserException;
 import lang.parser.interfaces.TypedStatementParser;
 import lang.parser.interfaces.ExpressionParser;
 import lang.parser.precedence.Precedence;
-
+import java.util.Set;
 import lang.token.*;
 
 /**
@@ -33,17 +36,68 @@ public class ExpressionStatementParser implements TypedStatementParser<Expressio
         TokenStream tokens = context.getTokenStream();
         Token token = tokens.getCurrentToken();
 
+        if (context.getTokenStream().isCurrentToken(TokenType.IDENTIFIER)
+                && isCompoundAssignmentOperator(tokens.getPeekToken())) {
+            return parseCompoundAssignment(context);
+        }
+
         Expression expression = expressionParser.parseExpression(context,
                 Precedence.LOWEST);
-
-        if (expression == null) {
-            throw new ParserException("Expected expression", token);
-        }
 
         if (tokens.isCurrentToken(TokenType.SEMICOLON)) {
             context.consumeCurrentToken(TokenType.SEMICOLON);
         }
 
         return new ExpressionStatement(token, expression);
+    }
+
+    private ExpressionStatement parseCompoundAssignment(ParsingContext context) {
+        Token starToken = context.consumeCurrentToken(TokenType.IDENTIFIER,
+                "Expected identifier at start of compound statement");
+
+        Identifier identifier = new Identifier(starToken, starToken.literal());
+
+        Token operatorToken = context.getTokenStream().getCurrentToken();
+        String operator = getBaseOperator(operatorToken);
+        context.consumeCurrentToken(operatorToken.type(), "Expected operator after identifier");
+
+        Expression right = expressionParser.parseExpression(context, Precedence.LOWEST);
+        context.consumeCurrentToken(TokenType.SEMICOLON, "Expected semicolon after compound statement");
+
+        InfixExpression infixExpression = new InfixExpression(
+                operatorToken,
+                identifier,
+                operator,
+                right);
+        AssignmentExpression assignmentExpression = new AssignmentExpression(operatorToken, identifier,
+                infixExpression);
+        return new ExpressionStatement(operatorToken, assignmentExpression);
+    }
+
+    private boolean isCompoundAssignmentOperator(Token token) {
+        Set<TokenType> operators = Set.of(TokenType.PLUS_ASSIGN,
+                TokenType.MINUS_ASSIGN,
+                TokenType.ASTERISK_ASSIGN,
+                TokenType.SLASH_ASSIGN,
+                TokenType.MODULUS_ASSIGN);
+
+        return operators.contains(token.type());
+    }
+
+    private String getBaseOperator(Token token) {
+        switch (token.type()) {
+            case PLUS_ASSIGN:
+                return "+";
+            case MINUS_ASSIGN:
+                return "-";
+            case ASTERISK_ASSIGN:
+                return "*";
+            case SLASH_ASSIGN:
+                return "/";
+            case MODULUS_ASSIGN:
+                return "%";
+            default:
+                throw new ParserException("Invalid compound assignment operator", token);
+        }
     }
 }

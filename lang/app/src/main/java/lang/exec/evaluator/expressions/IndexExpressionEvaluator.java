1dc0bf2a682602e23e5e5e71acd292cff7650cd2
Utkarsh Priyadarshi <utkarshpriyadarshi5026@gmail.com>
2025-07-18 16:29:47 +0530

    Refactor IndexExpressionEvaluator to utilize ErrorFactory for error handling: Updated error reporting for unsupported index types and out-of-bounds access to improve consistency and maintainability in error messaging.

--- lang/app/src/main/java/lang/exec/evaluator/expressions/IndexExpressionEvaluator.java
+++ lang/app/src/main/java/lang/exec/evaluator/expressions/IndexExpressionEvaluator.java
@@ -5,12 +5,12 @@ import lang.exec.objects.Environment;
 import lang.exec.evaluator.base.EvaluationContext;
 import lang.ast.expressions.IndexExpression;
 import lang.exec.base.BaseObject;
-import lang.exec.objects.ErrorObject;
+
 import lang.exec.objects.ArrayObject;
 import lang.exec.validator.ObjectValidator;
 import lang.exec.objects.HashObject;
 import lang.exec.objects.NullObject;
-
+import lang.exec.objects.errors.ErrorFactory;
 import lang.ast.base.Expression;
 
 public class IndexExpressionEvaluator implements NodeEvaluator<IndexExpression> {
@@ -30,7 +30,7 @@ public class IndexExpressionEvaluator implements NodeEvaluator<IndexExpression> 
             return evalHashIndexExpression(left, node.getIndex(), env, context);
         }
 
-        return new ErrorObject("Index operator not supported for type: " + left.type());
+        return ErrorFactory.indexNotSupported(left.type());
     }
 
     private BaseObject evalArrayIndexExpression(BaseObject array, Expression index, Environment env,
@@ -39,15 +39,13 @@ public class IndexExpressionEvaluator implements NodeEvaluator<IndexExpression> 
         BaseObject indexObject = context.evaluate(index, env);
 
         if (!ObjectValidator.isInteger(indexObject)) {
-            return new ErrorObject(String.format("Index must be an integer literal, got: %s", indexObject.inspect()));
+            return ErrorFactory.typeMismatch(array.type(), "array index", indexObject.type());
         }
 
         int arrIndex = (int) ObjectValidator.asInteger(indexObject).getValue();
 
         if (arrIndex < 0 || arrIndex >= arrayObject.getElements().size()) {
-            return new ErrorObject(
-                    String.format("Index out of bounds: %d for array of size %d", arrIndex,
-                            arrayObject.getElements().size()));
+            return ErrorFactory.indexOutOfBounds(arrIndex, arrayObject.getElements().size());
         }
 
         return arrayObject.getElements().get(arrIndex);
@@ -59,8 +57,7 @@ public class IndexExpressionEvaluator implements NodeEvaluator<IndexExpression> 
         BaseObject indexObject = context.evaluate(index, env);
 
         if (!ObjectValidator.isString(indexObject) && !ObjectValidator.isInteger(indexObject)) {
-            return new ErrorObject(
-                    String.format("Index must be a string or integer literal, got: %s", indexObject.inspect()));
+            return ErrorFactory.typeMismatch(hash.type(), "hash index", indexObject.type());
         }
 
         String key = ObjectValidator.isString(indexObject) ? ObjectValidator.asString(indexObject).getValue()

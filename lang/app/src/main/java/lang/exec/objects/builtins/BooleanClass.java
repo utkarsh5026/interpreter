eab89b8f0b8aed09009c707f2ac7d4bc91e3c51b
Utkarsh Priyadarshi <utkarshpriyadarshi5026@gmail.com>
2025-07-26 04:46:08 +0530

    Add FloatClass and IntegerClass implementations: Introduced built-in Float and Integer type classes with methods for mathematical operations, type conversion, and special value handling. This enhances the language's numeric capabilities and establishes a comprehensive type hierarchy for floating-point and integer types.

---
+++ lang/app/src/main/java/lang/exec/objects/builtins/BooleanClass.java
@@ -0,0 +1,196 @@
+package lang.exec.objects.builtins;
+
+import java.util.Optional;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Collections;
+import java.util.List;
+
+import lang.exec.objects.classes.*;
+import lang.exec.objects.base.BaseObject;
+import lang.exec.objects.env.*;
+import lang.exec.objects.error.ErrorObject;
+import lang.exec.objects.literals.*;
+import lang.exec.validator.ObjectValidator;
+
+/**
+ * ✅ BooleanClass - Built-in Boolean Type Class ✅
+ */
+class BooleanClass extends ClassObject {
+
+    public static final String BOOLEAN_CLASS_NAME = "Boolean";
+    private static BooleanClass instance;
+
+    private BooleanClass() {
+        super(
+                BOOLEAN_CLASS_NAME,
+                Optional.of(BaseObjectClass.getInstance()),
+                Optional.empty(),
+                createBooleanMethods(),
+                new Environment());
+    }
+
+    public static BooleanClass getInstance() {
+        if (instance == null) {
+            instance = new BooleanClass();
+        }
+        return instance;
+    }
+
+    private static Map<String, MethodObject> createBooleanMethods() {
+        var env = EnvironmentFactory.empty();
+        Map<String, MethodObject> methods = new HashMap<>();
+
+        methods.put("toString", createBooleanToStringMethod(env));
+        methods.put("__eq__", createBooleanEqMethod(env));
+        methods.put("__ne__", createBooleanNeMethod(env));
+        methods.put("__and__", createBooleanAndMethod(env));
+        methods.put("__or__", createBooleanOrMethod(env));
+        methods.put("__not__", createBooleanNotMethod(env));
+
+        return methods;
+    }
+
+    private static MethodObject createBooleanToStringMethod(Environment env) {
+        return new BuiltInMethod(
+                "toString",
+                "Returns string representation of this boolean",
+                Collections.emptyList(),
+                (instance, args) -> {
+                    if (args.length != 0) {
+                        return new ErrorObject("toString() takes no arguments, got " + args.length);
+                    }
+
+                    boolean value = getBooleanValue(instance);
+                    return StringClass.createStringInstance(String.valueOf(value));
+                },
+                env);
+    }
+
+    private static MethodObject createBooleanEqMethod(Environment env) {
+        return new BuiltInMethod(
+                "__eq__",
+                "Equality operator for booleans",
+                List.of("other"),
+                (instance, args) -> {
+                    if (args.length != 1) {
+                        return new ErrorObject("__eq__() takes exactly 1 argument, got " + args.length);
+                    }
+
+                    boolean leftValue = getBooleanValue(instance);
+
+                    if (ObjectValidator.isBoolean(args[0])) {
+                        boolean rightValue = getBooleanValue(args[0]);
+                        return createBooleanInstance(leftValue == rightValue);
+                    }
+
+                    return createBooleanInstance(false);
+                },
+                env);
+    }
+
+    private static MethodObject createBooleanNeMethod(Environment env) {
+        return new BuiltInMethod(
+                "__ne__",
+                "Inequality operator for booleans",
+                List.of("other"),
+                (instance, args) -> {
+                    if (args.length != 1) {
+                        return new ErrorObject("__ne__() takes exactly 1 argument, got " + args.length);
+                    }
+
+                    boolean leftValue = getBooleanValue(instance);
+
+                    if (ObjectValidator.isBoolean(args[0])) {
+                        boolean rightValue = getBooleanValue(args[0]);
+                        return createBooleanInstance(leftValue != rightValue);
+                    }
+
+                    return createBooleanInstance(true);
+                },
+                env);
+    }
+
+    private static MethodObject createBooleanAndMethod(Environment env) {
+        return new BuiltInMethod(
+                "__and__",
+                "Logical AND operator for booleans",
+                List.of("other"),
+                (instance, args) -> {
+                    if (args.length != 1) {
+                        return new ErrorObject("__and__() takes exactly 1 argument, got " + args.length);
+                    }
+
+                    boolean leftValue = getBooleanValue(instance);
+
+                    if (ObjectValidator.isBoolean(args[0])) {
+                        boolean rightValue = getBooleanValue(args[0]);
+                        return createBooleanInstance(leftValue && rightValue);
+                    }
+
+                    return new ErrorObject("Cannot perform logical AND with boolean and " + args[0].type());
+                },
+                env);
+    }
+
+    private static MethodObject createBooleanOrMethod(Environment env) {
+        return new BuiltInMethod(
+                "__or__",
+                "Logical OR operator for booleans",
+                List.of("other"),
+                (instance, args) -> {
+                    if (args.length != 1) {
+                        return new ErrorObject("__or__() takes exactly 1 argument, got " + args.length);
+                    }
+
+                    boolean leftValue = getBooleanValue(instance);
+
+                    if (ObjectValidator.isBoolean(args[0])) {
+                        boolean rightValue = getBooleanValue(args[0]);
+                        return createBooleanInstance(leftValue || rightValue);
+                    }
+
+                    return new ErrorObject("Cannot perform logical OR with boolean and " + args[0].type());
+                },
+                env);
+    }
+
+    private static MethodObject createBooleanNotMethod(Environment env) {
+        return new BuiltInMethod(
+                "__not__",
+                "Logical NOT operator for booleans",
+                Collections.emptyList(),
+                (instance, args) -> {
+                    if (args.length != 0) {
+                        return new ErrorObject("__not__() takes no arguments, got " + args.length);
+                    }
+
+                    boolean value = getBooleanValue(instance);
+                    return createBooleanInstance(!value);
+                },
+                env);
+    }
+
+    private static boolean getBooleanValue(BaseObject obj) {
+        if (ObjectValidator.isInstance(obj)) {
+            InstanceObject instance = ObjectValidator.asInstance(obj);
+            Optional<BaseObject> valueProperty = instance.getProperty("value");
+            if (valueProperty.isPresent() && ObjectValidator.isBoolean(valueProperty.get())) {
+                return ObjectValidator.asBoolean(valueProperty.get()).getValue();
+            }
+        }
+        if (ObjectValidator.isBoolean(obj)) {
+            return ObjectValidator.asBoolean(obj).getValue();
+        }
+        return false;
+    }
+
+    /**
+     * 🏗️ Creates a Boolean instance with the given value
+     */
+    public static InstanceObject createBooleanInstance(boolean value) {
+        InstanceObject instance = BooleanClass.getInstance().createInstance();
+        instance.setProperty("value", new BooleanObject(value));
+        return instance;
+    }
+}

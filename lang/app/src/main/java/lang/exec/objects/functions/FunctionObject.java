a106666cb83099313e73320065a4256016d34bed
Utkarsh Priyadarshi <utkarshpriyadarshi5026@gmail.com>
2025-07-26 01:46:39 +0530

    Add execute method to FunctionObject: Implemented the execute method to handle function invocation with argument validation and error handling. Introduced EvaluationContext for context management and ObjectValidator for result validation, enhancing the function execution process and improving error reporting.

--- lang/app/src/main/java/lang/exec/objects/functions/FunctionObject.java
+++ lang/app/src/main/java/lang/exec/objects/functions/FunctionObject.java
@@ -6,6 +6,8 @@ import lang.ast.statements.BlockStatement;
 import lang.ast.base.Identifier;
 import lang.exec.objects.base.*;
 import lang.exec.objects.env.Environment;
+import lang.exec.evaluator.base.EvaluationContext;
+import lang.exec.validator.ObjectValidator;
 
 /**
  * FunctionObject represents a function in the language.
@@ -45,6 +47,35 @@ public class FunctionObject implements BaseObject {
     @Override
     public ObjectType type() {
         return ObjectType.FUNCTION;
+    }
+
+    /**
+     * Executes the function with the given arguments and returns the result.
+     */
+    public BaseObject execute(BaseObject[] arguments, EvaluationContext context) {
+        Environment extendedEnv = new Environment(environment, false);
+
+        if (parameters.size() != arguments.length) {
+            return context.createError(String.format("Wrong number of arguments. Expected %d, got %d",
+                    parameters.size(), arguments.length), body.position());
+        }
+
+        for (int i = 0; i < parameters.size(); i++) {
+            String paramName = parameters.get(i).getValue();
+            extendedEnv.defineVariable(paramName, arguments[i]);
+        }
+
+        BaseObject result = context.evaluate(body, extendedEnv);
+
+        if (ObjectValidator.isError(result)) {
+            return result;
+        }
+
+        if (ObjectValidator.isReturnValue(result)) {
+            return ObjectValidator.asReturnValue(result).getValue();
+        }
+
+        return result;
     }
 
 }

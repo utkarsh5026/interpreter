b6f1c85feb0c63f5292eb08bdcf00923d37c1450
Utkarsh Priyadarshi <utkarshpriyadarshi5026@gmail.com>
2025-07-26 04:58:56 +0530

    Refactor StringLiteralEvaluator to use StringClass: Updated the evaluate method to create string instances using StringClass instead of StringObject, enhancing the integration of string handling in the evaluation process.

--- lang/app/src/main/java/lang/exec/evaluator/literals/StringLiteralEvaluator.java
+++ lang/app/src/main/java/lang/exec/evaluator/literals/StringLiteralEvaluator.java
@@ -3,14 +3,14 @@ package lang.exec.evaluator.literals;
 import lang.exec.evaluator.base.EvaluationContext;
 import lang.exec.evaluator.base.NodeEvaluator;
 import lang.exec.objects.base.BaseObject;
+import lang.exec.objects.builtins.StringClass;
 import lang.exec.objects.env.Environment;
-import lang.exec.objects.literals.StringObject;
 import lang.ast.literals.StringLiteral;
 
 public class StringLiteralEvaluator implements NodeEvaluator<StringLiteral> {
 
     @Override
     public BaseObject evaluate(StringLiteral node, Environment env, EvaluationContext context) {
-        return new StringObject(node.getValue());
+        return StringClass.createStringInstance(node.getValue());
     }
 }

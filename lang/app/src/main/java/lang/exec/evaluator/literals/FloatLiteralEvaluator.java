15c72e3538d1b185eb154be494e35bb276dbf5ff
Utkarsh Priyadarshi <utkarshpriyadarshi5026@gmail.com>
2025-07-26 13:42:55 +0530

    Refactor FloatLiteralEvaluator to use FloatClass: Updated the evaluate method to create float instances using FloatClass instead of FloatObject, enhancing consistency in float value handling.

--- lang/app/src/main/java/lang/exec/evaluator/literals/FloatLiteralEvaluator.java
+++ lang/app/src/main/java/lang/exec/evaluator/literals/FloatLiteralEvaluator.java
@@ -3,8 +3,8 @@ package lang.exec.evaluator.literals;
 import lang.exec.evaluator.base.EvaluationContext;
 import lang.exec.evaluator.base.NodeEvaluator;
 import lang.exec.objects.base.BaseObject;
+import lang.exec.objects.builtins.FloatClass;
 import lang.exec.objects.env.Environment;
-import lang.exec.objects.literals.FloatObject;
 import lang.ast.literals.FloatLiteral;
 
 public class FloatLiteralEvaluator implements NodeEvaluator<FloatLiteral> {
@@ -12,6 +12,6 @@ public class FloatLiteralEvaluator implements NodeEvaluator<FloatLiteral> {
     @Override
     public BaseObject evaluate(FloatLiteral node, Environment env, EvaluationContext context) {
         double value = node.getValue();
-        return new FloatObject(value);
+        return FloatClass.createFloatInstance(value);
     }
 }

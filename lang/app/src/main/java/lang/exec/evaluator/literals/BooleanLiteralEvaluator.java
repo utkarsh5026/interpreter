38119b321aea1f0750c3ae3d965b7df833f4e5fe
Utkarsh Priyadarshi <utkarshpriyadarshi5026@gmail.com>
2025-07-26 12:33:12 +0530

    Refactor BooleanLiteralEvaluator to use BooleanClass: Updated the evaluate method to create boolean instances using BooleanClass instead of BooleanObject, improving consistency in boolean value handling.

--- lang/app/src/main/java/lang/exec/evaluator/literals/BooleanLiteralEvaluator.java
+++ lang/app/src/main/java/lang/exec/evaluator/literals/BooleanLiteralEvaluator.java
@@ -4,13 +4,13 @@ import lang.exec.evaluator.base.EvaluationContext;
 import lang.exec.evaluator.base.NodeEvaluator;
 import lang.exec.objects.base.BaseObject;
 import lang.exec.objects.env.Environment;
-import lang.exec.objects.literals.BooleanObject;
 import lang.ast.expressions.BooleanExpression;
+import lang.exec.objects.builtins.BooleanClass;
 
 public class BooleanLiteralEvaluator implements NodeEvaluator<BooleanExpression> {
 
     @Override
     public BaseObject evaluate(BooleanExpression node, Environment env, EvaluationContext context) {
-        return new BooleanObject(node.getValue());
+        return BooleanClass.createBooleanInstance(node.getValue());
     }
 }

ab8f212fbf0597f23ff3f631c149c0841f33f60e
Utkarsh Priyadarshi <utkarshpriyadarshi5026@gmail.com>
2025-07-26 12:39:16 +0530

    Refactor IntegerLiteralEvaluator to use IntegerClass: Updated the evaluate method to create integer instances using IntegerClass instead of IntegerObject, improving consistency in integer value handling.

--- lang/app/src/main/java/lang/exec/evaluator/literals/IntegerLiteralEvaluator.java
+++ lang/app/src/main/java/lang/exec/evaluator/literals/IntegerLiteralEvaluator.java
@@ -3,14 +3,14 @@ package lang.exec.evaluator.literals;
 import lang.exec.evaluator.base.EvaluationContext;
 import lang.exec.evaluator.base.NodeEvaluator;
 import lang.exec.objects.base.BaseObject;
+import lang.exec.objects.builtins.IntegerClass;
 import lang.exec.objects.env.Environment;
-import lang.exec.objects.literals.IntegerObject;
 import lang.ast.literals.IntegerLiteral;
 
 public class IntegerLiteralEvaluator implements NodeEvaluator<IntegerLiteral> {
 
     @Override
     public BaseObject evaluate(IntegerLiteral node, Environment env, EvaluationContext context) {
-        return new IntegerObject(node.getValue());
+        return IntegerClass.createIntegerInstance(node.getValue());
     }
 }

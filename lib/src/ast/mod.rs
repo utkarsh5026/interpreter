fd82a6439f7ff484da05ff9e919398037ac07004
Utkarsh Priyadarshi <utkarshpriyadarshi5026@gmail.com>
2026-04-04 03:22:49 +0530

    feat(ast): expand Expression/Statement factory API and make constructors public
    Add factory constructors for function, index, this, call, new, super, if,
    and prefix expressions. Drop redundant value params from boolean, string,
    and identifier constructors (values are already encoded in the TokenSpan).
    Promote all Statement factories from pub(crate) to pub. Add end_pos() to
    TokenSpan, derive Debug, and annotate pure methods with #[must_use].

--- lib/src/ast/mod.rs
+++ lib/src/ast/mod.rs
@@ -18,22 +18,31 @@ pub mod expression;
 pub mod literal;
 pub mod statements;
 
+#[derive(Debug)]
 pub struct TokenSpan {
     start: Token,
     end: TokenPosition,
 }
 
 impl TokenSpan {
+    #[must_use]
     pub const fn new(start: Token, end: TokenPosition) -> Self {
         Self { start, end }
     }
 
+    #[must_use]
     pub fn literal(&self) -> &str {
         &self.start.literal
     }
 
-    pub fn start_pos(&self) -> &TokenPosition {
+    #[must_use]
+    pub const fn start_pos(&self) -> &TokenPosition {
         &self.start.position
+    }
+
+    #[must_use]
+    pub const fn end_pos(&self) -> &TokenPosition {
+        &self.end
     }
 }
 
@@ -48,32 +57,51 @@ impl Expression {
         Self::Literal(Literal::Null(NullLitreal::new(span.into())))
     }
 
+    pub fn function(
+        span: impl Into<TokenSpan>,
+        params: Vec<Indentifier>,
+        body: statements::BlockStatement,
+    ) -> Self {
+        Self::Literal(Literal::Func(FunctionLiteral::new(
+            span.into(),
+            params,
+            body,
+        )))
+    }
+
+    pub fn index(span: impl Into<TokenSpan>, left: Self, index: Self) -> Self {
+        Self::Index(expression::IndexExpression::new(
+            span.into(),
+            Box::new(left),
+            Box::new(index),
+        ))
+    }
+
+    pub fn this(span: impl Into<TokenSpan>) -> Self {
+        Self::This(expression::ThisExpression::new(span.into()))
+    }
+
     pub fn integer(span: impl Into<TokenSpan>, value: i64) -> Self {
         Self::Literal(Literal::Integer(IntegerLiteral::new(span.into(), value)))
     }
 
-    pub fn boolean(span: impl Into<TokenSpan>, value: bool) -> Self {
-        Self::Literal(Literal::Bool(BooleanLiteral::new(span.into(), value)))
+    pub fn boolean(span: impl Into<TokenSpan>) -> Self {
+        Self::Literal(Literal::Bool(BooleanLiteral::new(span.into())))
     }
 
-    pub fn string(span: impl Into<TokenSpan>, value: String) -> Self {
-        Self::Literal(Literal::String(StringLiteral::new(span.into(), value)))
+    pub fn string(span: impl Into<TokenSpan>) -> Self {
+        Self::Literal(Literal::String(StringLiteral::new(span.into())))
     }
 
-    pub fn array(span: impl Into<TokenSpan>, elements: Vec<Expression>) -> Self {
+    pub fn array(span: impl Into<TokenSpan>, elements: Vec<Self>) -> Self {
         Self::Literal(Literal::Array(ArrayLiteral::new(span.into(), elements)))
     }
 
-    pub fn identifier(span: impl Into<TokenSpan>, value: String) -> Self {
-        Self::Identifier(Indentifier::new(span.into(), value))
+    pub fn identifier(span: impl Into<TokenSpan>) -> Self {
+        Self::Identifier(Indentifier::new(span.into()))
     }
 
-    pub fn infix(
-        span: impl Into<TokenSpan>,
-        left: Expression,
-        right: Expression,
-        operator: Operator,
-    ) -> Self {
+    pub fn infix(span: impl Into<TokenSpan>, left: Self, right: Self, operator: Operator) -> Self {
         Self::Infix(expression::InfixExpression::new(
             span.into(),
             Box::new(left),
@@ -82,17 +110,59 @@ impl Expression {
         ))
     }
 
-    pub fn assignment(span: impl Into<TokenSpan>, name: Expression, value: Expression) -> Self {
+    pub fn assignment(span: impl Into<TokenSpan>, name: Self, value: Self) -> Self {
         Self::Assignment(expression::AssignmentExpression::new(
             span.into(),
             Box::new(name),
             Box::new(value),
         ))
     }
+
+    pub fn call(span: impl Into<TokenSpan>, function: Self, args: Vec<Self>) -> Self {
+        Self::Call(expression::CallExpression::new(
+            span.into(),
+            Box::new(function),
+            args,
+        ))
+    }
+
+    pub fn new_expr(span: impl Into<TokenSpan>, class_name: Self, args: Vec<Self>) -> Self {
+        Self::New(expression::NewExpression::new(
+            span.into(),
+            Box::new(class_name),
+            args,
+        ))
+    }
+
+    pub fn super_expr(span: impl Into<TokenSpan>, method: Option<Self>, args: Vec<Self>) -> Self {
+        Self::Super(expression::SuperExpression::new(span.into(), method, args))
+    }
+
+    pub fn if_expr(
+        span: impl Into<TokenSpan>,
+        conditions: Vec<Self>,
+        consequences: Vec<statements::BlockStatement>,
+        alternative: Option<statements::BlockStatement>,
+    ) -> Self {
+        Self::If(expression::IfExpression::new(
+            span.into(),
+            conditions,
+            consequences,
+            alternative,
+        ))
+    }
+
+    pub fn prefix(span: impl Into<TokenSpan>, operator: Operator, right: Self) -> Self {
+        Self::Prefix(expression::PrefixExpression::new(
+            span.into(),
+            operator,
+            Box::new(right),
+        ))
+    }
 }
 
 impl Statement {
-    pub(crate) fn let_stmt(
+    pub fn let_stmt(
         span: impl Into<TokenSpan>,
         name: Indentifier,
         value: expression::Expression,
@@ -100,7 +170,7 @@ impl Statement {
         Self::Let(LetStatement::new(span.into(), name, value))
     }
 
-    pub(crate) fn const_stmt(
+    pub fn const_stmt(
         span: impl Into<TokenSpan>,
         name: Indentifier,
         value: expression::Expression,
@@ -108,22 +178,19 @@ impl Statement {
         Self::Const(ConstStatement::new(span.into(), name, value))
     }
 
-    pub(crate) fn return_stmt(span: impl Into<TokenSpan>, value: expression::Expression) -> Self {
+    pub fn return_stmt(span: impl Into<TokenSpan>, value: expression::Expression) -> Self {
         Self::Return(ReturnStatement::new(span.into(), value))
     }
 
-    pub(crate) fn expression_stmt(
-        span: impl Into<TokenSpan>,
-        expr: expression::Expression,
-    ) -> Self {
+    pub fn expression_stmt(span: impl Into<TokenSpan>, expr: expression::Expression) -> Self {
         Self::Expression(ExpressionStatement::new(span.into(), expr))
     }
 
-    pub(crate) fn block(span: impl Into<TokenSpan>, stmts: Vec<Statement>) -> Self {
+    pub fn block(span: impl Into<TokenSpan>, stmts: Vec<Self>) -> Self {
         Self::Block(BlockStatement::new(span.into(), stmts))
     }
 
-    pub(crate) fn while_stmt(
+    pub fn while_stmt(
         span: impl Into<TokenSpan>,
         condition: expression::Expression,
         body: BlockStatement,
@@ -131,31 +198,31 @@ impl Statement {
         Self::While(WhileStatement::new(span.into(), condition, body))
     }
 
-    pub(crate) fn for_stmt(
+    pub fn for_stmt(
         span: impl Into<TokenSpan>,
-        initializer: Box<Statement>,
+        initializer: Self,
         condition: expression::Expression,
         increment: expression::Expression,
         body: BlockStatement,
     ) -> Self {
         Self::For(ForStatement::new(
             span.into(),
-            initializer,
+            Box::new(initializer),
             condition,
             increment,
             body,
         ))
     }
 
-    pub(crate) fn break_stmt(span: impl Into<TokenSpan>) -> Self {
+    pub fn break_stmt(span: impl Into<TokenSpan>) -> Self {
         Self::Break(BreakStatement::new(span.into()))
     }
 
-    pub(crate) fn continue_stmt(span: impl Into<TokenSpan>) -> Self {
+    pub fn continue_stmt(span: impl Into<TokenSpan>) -> Self {
         Self::Continue(ContinueStatement::new(span.into()))
     }
 
-    pub(crate) fn class_stmt(
+    pub fn class_stmt(
         span: impl Into<TokenSpan>,
         name: Indentifier,
         parent: Option<Indentifier>,
